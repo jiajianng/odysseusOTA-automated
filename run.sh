75bd8f72274a1589ecf3311fa852484ee7d7e19d
JJGadgets <jiajianng@me.com>
2015-10-24 23:54:37 +0800

    Whoops (2)
    forgot about that line… such an idiot

--- run.sh
+++ run.sh
@@ -18,7 +18,6 @@
 read -p "Welcome to odysseusOTA! Are you sure you want to downgrade your iPhone4,1/iPhone 4S to iOS 6.1.3? Press [ENTER] to continue/say yes, to exit just close out this window/tab."
 echo
 cd macos
-read -p Oh, right! Please make sure you downloaded the latest version of odysseusOTA and you have not changed anything. Press [ENTER] if you comply with the above condition, or else GET OUTTA HERE NOW!
 read -p "Oh, right! Please make sure you downloaded the latest version of odysseusOTA and you have not changed anything. Press [ENTER] if you comply with the above condition, or else GET OUTTA HERE NOW!"
 read -p "Make sure you have the original 6.1.3 IPSW in the macos folder."
 echo We are now going to build the custom downgrade IPSW.
