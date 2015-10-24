0ed4f20afa0142df3ae21b1366645753969c2fdd
JJGadgets <jiajianng@me.com>
2015-10-24 23:53:33 +0800

    Fix existing issues I found
    Back from dinner, only to find out I am a scripting noob when I ran the
    previous script… lol

--- run.sh
+++ run.sh
@@ -11,10 +11,14 @@
 
 
 
-read -p Welcome to odysseusOTA! Are you sure you want to downgrade your iPhone4,1/iPhone 4S to iOS 6.1.3? Press [ENTER] to continue/say yes, to exit just close out this window/tab.
+#echo Welcome to odysseusOTA
+#echo We are now going to build the custom downgrade IPSW.
+#display dialog
+
+read -p "Welcome to odysseusOTA! Are you sure you want to downgrade your iPhone4,1/iPhone 4S to iOS 6.1.3? Press [ENTER] to continue/say yes, to exit just close out this window/tab."
 echo
 cd macos
 read -p Oh, right! Please make sure you downloaded the latest version of odysseusOTA and you have not changed anything. Press [ENTER] if you comply with the above condition, or else GET OUTTA HERE NOW!
-read -p Make sure you have the original 6.1.3 IPSW in the macos folder.
+read -p "Oh, right! Please make sure you downloaded the latest version of odysseusOTA and you have not changed anything. Press [ENTER] if you comply with the above condition, or else GET OUTTA HERE NOW!"
+read -p "Make sure you have the original 6.1.3 IPSW in the macos folder."
 echo We are now going to build the custom downgrade IPSW.
-display dialog 
